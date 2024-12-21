46c20d22e7ba87b625c126d42a17684c45303dda
ThomasLaforge <thomas.laforge.38@gmail.com>
2024-12-21 13:45:54 +0100

    add powers

---
+++ src/modules/enums.ts
@@ -0,0 +1,14 @@
+export enum Power {
+  Fox = "Fox",
+  FreeAction = "FreeAction",
+  YellowCheck = "YellowCheck",
+  BlueCheck = "blueCheck",
+  GreenCheck = "greenCheck",
+  PurpleSix = "PurpleSix",
+  ReRoll = "ReRoll",
+  OrangeFour = "OrangeFour",
+  OrangeFive = "OrangeFive",
+  OrangeSix = "OrangeSix",
+  Double = "Double",
+  Triple = "Triple"
+}
